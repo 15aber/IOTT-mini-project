a0a592ae66956fba890f4fb03f87dbe7888a9b09
Simon Hansen <sim.hansen@hotmail.com>
2018-11-09 11:44:59 +0100

    Charts

--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -2,14 +2,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from mdata.views import get_data
-from mdata.views import webhook_receiver
+from mdata.views import webhook_receiver, ChartData, HomeView
 
 
 urlpatterns = [
-    path('', include('mdata.urls')),
+    path('', HomeView.as_view(), name='home'),
     path('admin/', admin.site.urls),
     path('mdata/', include('mdata.urls')),
     path('api/data/', get_data, name='api-data'),
     path('webhook/', webhook_receiver, name='webhook_receiver'),
+    path('api/chart/data/', ChartData.as_view())
 
 ]
